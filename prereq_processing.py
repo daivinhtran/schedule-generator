bc178d84fb65263f558e055505b44c7a44a1ee4f
Vinh Tran <daivinhtran.vt@gmail.com>
2018-03-24 12:13:27 -0400

    ..

--- prereq_processing.py
+++ prereq_processing.py
@@ -1,21 +1,48 @@
+def parser(s):
+  coursePrefix = ['MATH', 'CS', 'ECE', 'CX', 'ISYE', 'BMED', 'CEE']
+  res = []
+  s = s.split(' ')
+  i = 0
+  while i < len(s):
+    if s[i] == '(':
+      t = []
+      i += 1
+      while s[i] != ')':
+        if s[i] in coursePrefix:
+          t.append(s[i] + s[i+1])
+          i += 1
+        elif s[i] == 'OR' or s[i] == 'AND':
+          t.append(s[i])
+        i += 1
+      res.append(t)
+    elif s[i] in coursePrefix:
+      res.append(s[i] + s[i+1])
+      i += 1
+    elif s[i] == 'OR' or s[i] == 'AND':
+      res.append(s[i])
+    i += 1
+
+  return res
+
 file = open("prereq.txt", "r")
 prereq = {}
 specialChars = ['(', 'CS', 'MATH']
 
 for line in file.readlines():
   # clean data
+  line = line.replace('CSCL', '')
   line = line.replace('"C" or higher ', '')
   line = line.replace('Bus Process Analy/Design ', '')
+  line = line.replace('Math', 'MATH')
 
   # get course name
   spl = line.split(' ')
   courseName = spl[0] + spl[1]
   
-
   line = ' '.join(spl[2:])
 
   if 'None' in line:
-    print(courseName, 'None')
+    prereq[courseName] = []
     continue
 
   for i in range(len(line) - 5):
@@ -32,9 +59,13 @@ for line in file.readlines():
       line = line[i:]
       break
 
-
   line = line[:-1]
+  line = line.replace('(', '( ')
+  line = line.replace(')', ' )')
+  line = line.replace(';', ' ;')
+  line = line.replace('or', 'OR')
+  line = line.replace('and', 'AND')
+  prereq[courseName] = parser(line)
 
-
-  print(courseName, line)
-
+for course in prereq:
+  print(course, prereq[course])
