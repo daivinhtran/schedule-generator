2a170ce6866f74949f3f030f4c86f14e8c4f3723
Vinh Tran <daivinhtran.vt@gmail.com>
2018-03-24 00:17:01 -0400

    third commit

--- web-scraper/PreReq.py
+++ web-scraper/PreReq.py
@@ -1,18 +1,40 @@
 file = open("prereq.txt", "r")
+prereq = {}
+specialChars = ['(', 'CS', 'MATH']
 
 for line in file.readlines():
-  print(line)
+  # clean data
+  line = line.replace('"C" or higher ', '')
+  line = line.replace('Bus Process Analy/Design', '')
+
+  # get course name
+  spl = line.split(' ')
+  courseName = spl[0] + spl[1]
+  
+
+  line = ' '.join(spl[2:])
+
+  if 'None' in line:
+    print(courseName, 'None')
+    continue
+
+  for i in range(len(line) - 5):
+    if line[i] == '(':
+      line = line[i:]
+      break
+    elif line[i:i+2] == 'CS':
+      line = line[i:]
+      break
+    elif line[i:i+3] == 'ECE':
+      line = line[i:]
+      break
+    elif line[i:i+4] == 'MATH':
+      line = line[i:]
+      break
 
 
-# build prereq hashmap
-prereq = {
-  cs1171: [cs1301, 'or', cs15xx, 'or', ]
-}
+  line = line[:-1]
 
 
-takenCourses = [....]
-requiredCoursesToGraduate = [....]
-nextSemesterCourses = generate(takenCourses, requiredCoursesToGraduate)
-requiredCoursesToGraduate = requiredCoursesToGraduate - nextSemesterCourses
-takenCourses = takenCourses + nextSemesterCourses
-nextSemesterCourses = generate(takenCourses, requiredCoursesToGraduate)
+  print(courseName, line)
+
